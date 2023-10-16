43a315c6d0b81c41eb1b82d5c3b9914e446047d1
Louie <louiesaguinsin@gmail.com>
2023-10-16 20:01:36 +0800

    t1t3

--- app/src/main/java/com/example/woki_toki/HomeActivity.java
+++ app/src/main/java/com/example/woki_toki/HomeActivity.java
@@ -2,11 +2,15 @@ package com.example.woki_toki;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
@@ -14,6 +18,9 @@ public class HomeActivity extends AppCompatActivity {
     String[] channel = {"Channel 1","Channel 2","Channel 3","Channel 4","Channel 5"};
 
     AutoCompleteTextView autoCompleteTextView;
+    ImageButton speakerbtn, bigwhitemic;
+    TextView talkstatus;
+    boolean isMuted,isTalking;
     ArrayAdapter<String> adapterItems;
 
     @Override
@@ -22,12 +29,16 @@ public class HomeActivity extends AppCompatActivity {
         // Hide the ActionBar/Toolbar
         getSupportActionBar().hide();
         setContentView(R.layout.activity_home);
+        speakerbtn = findViewById(R.id.speakerbtn);
+        bigwhitemic = findViewById(R.id.bigwhitemic);
+        talkstatus = findViewById(R.id.ttttext);
+
 
         autoCompleteTextView = findViewById(R.id.auto_complete_text);
         adapterItems = new ArrayAdapter<String>(this, R.layout.list_item, channel);
 
         autoCompleteTextView.setAdapter(adapterItems);
-
+        //CHANNEL SELECTION
         autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -35,5 +46,55 @@ public class HomeActivity extends AppCompatActivity {
                 Toast.makeText(HomeActivity.this, "Item: "+item, Toast.LENGTH_SHORT).show();
             }
         });
+        //END OF CHANNEL SELECTION
+        bigwhitemic.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                int action = event.getAction();
+                if (action == MotionEvent. ACTION_DOWN) {
+                    bigwhitemic.setImageDrawable(getResources().getDrawable(R.drawable.bigwhitemictalk));
+                    talkstatus.setText("Talk Now");
+                } else if (action == MotionEvent. ACTION_UP || action == MotionEvent. ACTION_CANCEL ){
+                    bigwhitemic.setImageDrawable(getResources().getDrawable(R.drawable.micbtn));
+                    talkstatus.setText("Tap to talk");
+                }
+                return false;
+            }
+        });
+        /*
+        //MIC BUTTON
+        bigwhitemic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isTalking){
+                    isTalking=true;
+                    bigwhitemic.setImageDrawable(getResources().getDrawable(R.drawable.bigwhitemictalk));
+                    talkstatus.setText("Talk Now");
+                }
+                else {
+                    isTalking=false;
+                    bigwhitemic.setImageDrawable(getResources().getDrawable(R.drawable.micbtn));
+                    talkstatus.setText("Tap to talk");
+                }
+            }
+        });
+        //END MIC BUTTON
+
+         */
+        //MUTE BUTTON
+        speakerbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isMuted){
+                    isMuted=true;
+                    speakerbtn.setImageDrawable(getResources().getDrawable(R.drawable.volmute));
+                }
+                else {
+                    isMuted=false;
+                    speakerbtn.setImageDrawable(getResources().getDrawable(R.drawable.volup));
+                }
+            }
+        });
+        //END MUTE BUTTON
     }
 }
